bd3c44740a2ec4a414e59e18ddd9328dd23cabe7
Rick T. Zhang <rtzhang@ucsb.edu>
2023-11-10 04:16:07 +0000

    Created working (for now) main.cpp file

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,24 @@
-
-
-
 #include <Arduino.h>
 #include <imxrt.h>
-void setup(){
 
+#include "enums.h"
+#include "fakenodes.cpp"
 
+States state;
 
-    
+void setup() {
+    state = OFF;  
 }
 
 void loop(){
-
-
-    
+    switch (state) {
+        case OFF:
+            state = off();
+        case ON:
+            state = on();
+        case DRIVE_READY:
+            state = drive_ready();
+        case D_PLAUSE:
+            staet = d_plaus();
+    }
 }
