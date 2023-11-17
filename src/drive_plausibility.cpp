ec57179877f240ed792c379c737920d9a1b2daff
vedantshah99 <vedantshah@ucsb.edu>
2023-11-16 19:45:45 -0800

    finished initial for on

--- src/drive_plausibility.cpp
+++ src/drive_plausibility.cpp
@@ -3,12 +3,9 @@
 #include <main.h>
 #include <fakenodes.h>
 
-// float throttle = 0.03;
-// float ADPS_implausibility = 0.1;
-
 int drive_plausibility(){
 
-    // cut_power();
+    // set current to 0;
 
     if( FAKENODES::throttle() < 0.05 && FAKENODES::adps_implausibility() < 0.1)
         return DRIVE_READY;
