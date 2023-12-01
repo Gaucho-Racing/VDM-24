829f2a15698f0da7360b8122a981cbd877d3598e
Rick T. Zhang <rtzhang@ucsb.edu>
2023-12-01 04:40:28 +0000

    included iCANflex &Car in all states

--- src/drive.cpp
+++ src/drive.cpp
@@ -1,16 +1,16 @@
 #include <Arduino.h>
 #include <imxrt.h>
-#include <main.h>
-#include <fakenodes.h>
+#include "main.h"
+#include "fakenodes.h"
 
-int drive(){
+int drive(iCANflex &Car){
 
     // if on and brake and drive
     //Fix units for brake pressure
     //on_switch and ready_to_drive are still fake nodes
     if(FAKENODES::on_switch() && 
-       Car.Pedals.getBrakePressureF() > 0.0 &&
-       Car.Pedals.getBrakePressureR() > 0.0 &&
+       Car.PEDALS.getBrakePressureF() > 0.0 &&
+       Car.PEDALS.getBrakePressureR() > 0.0 &&
        FAKENODES::ready_to_drive())
     {
         return DRIVE_READY;
