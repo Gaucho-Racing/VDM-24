58e7ab92c77696be5856b5db56e9b914e665f868
aneesh <garganeeshk@gmail.com>
2023-12-01 13:11:48 -0800

    basic check to change to ON state from OFF

--- src/off.cpp
+++ src/off.cpp
@@ -1,1 +1,15 @@
 // off.cpp
+
+#include <Arduino.h>
+#include <imxrt.h>
+#include <main.h>
+#include <fakenodes.h>
+
+States off(){
+
+    
+    if(FAKENODES::on() && !FAKENODES::drive()) { //there should also be a check for wrong, but we are ignoring that for now
+            return ON;
+        }
+
+}
