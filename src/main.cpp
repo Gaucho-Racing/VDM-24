e43240f2f2c87141501a2079524d4cc9e6e6b8af
Justin Wu <justin_wu@ucsb.edu>
2023-12-07 20:31:41 -0800

    add error handling for current limit (code 106)

--- src/main.cpp
+++ src/main.cpp
@@ -17,8 +17,23 @@ State sendToError(volatile State currentState, volatile bool (*erFunc)(iCANflex&
    return ERROR;
 }
 
+volatile bool currentLimitSafe(iCANflex& Car) {
+    return (Car.DTI.getDCCurrent() > 575); // based on below current limit
+}
+
+volatile bool currentLimitExceeded(iCANflex& Car) {
+    return (Car.DTI.getDCCurrent() > 600); // taken from last year's implementation
+}
+
+void currentLimitExceeded_ISR() {
+    Car.sendDashError(106);
+    state = sendToError(state, currentLimitSafe);
+}
 
 void loop(){
+    if (currentLimitExceeded(Car)) { 
+        NVIC_TRIGGER_IRQ(1); // pin number is filler
+    }
     switch (state) {
         case OFF:
             state = off(Car, switches);
@@ -45,6 +60,7 @@ void setup() {
     Serial.begin(9600);
     Car.begin();
 
+    attachInterruptVector(1, &currentLimitExceeded_ISR); // pin number is filler
 
     // set all the switchboard pins to  digital read inputs
 
