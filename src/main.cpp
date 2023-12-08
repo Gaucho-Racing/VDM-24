f90a55164e02c6434160a4c03f059efdde0cb53f
Karan Gumma <kgumma4@gmail.com>
2023-12-07 20:31:36 -0800

    can receive failure initial code

--- src/main.cpp
+++ src/main.cpp
@@ -17,8 +17,29 @@ State sendToError(volatile State currentState, volatile bool (*erFunc)(iCANflex&
    return ERROR;
 }
 
+void canReceiveFailure_ISR() {
+    Car.sendDashError(100);
+    state = sendToError(state, canReceiveFailure(Car));
+}
+
+const int canFailureThreshold = 100; // msec
+
+volatile bool canReceiveFailure(iCANflex& Car) {
+    return 
+        Car.Inverter.getAge() > canFailureThreshold ||
+        Car.VDM.getAge() > canFailureThreshold ||
+        Car.Wheel.getAge() > canFailureThreshold ||
+        Car.GPS.getAge() > canFailureThreshold ||
+        Car.Pedals.getAge() > canFailureThreshold ||
+        Car.ACU.getAge() > canFailureThreshold ||
+        Car.BCM.getAge() > canFailureThreshold ||
+        Car.Dash.getAge() > canFailureThreshold ||
+        Car.Energy_Meter.getAge() > canFailureThreshold;
+}
 
 void loop(){
+    if (canReceiveFailure(Car)) {NVIC_TRIGGER_IRQ(10);}
+
     switch (state) {
         case OFF:
             state = off(Car, switches);
@@ -42,6 +63,8 @@ void loop(){
 }
 
 void setup() {
+    attachInterruptVector(10, &canReceiveFailure_ISR);
+
     Serial.begin(9600);
     Car.begin();
 
